6919935ad30457c88f1071039c556c1fe6a314d3
t.urbutt <tim@urbutt.de>
2023-02-21 00:46:34 +0100

    update binaries to arm64

--- ios/TensorFlowLiteC.framework/Headers/TensorFlowLiteC.h
+++ ios/TensorFlowLiteC.framework/Headers/TensorFlowLiteC.h
@@ -1,3 +1,8 @@
-#import "c_api.h"
-#import "xnnpack_delegate.h"
-#import "common.h"
+#import <TensorFlowLiteC/builtin_ops.h>
+#import <TensorFlowLiteC/c_api.h>
+#import <TensorFlowLiteC/c_api_experimental.h>
+#import <TensorFlowLiteC/c_api_types.h>
+#import <TensorFlowLiteC/common.h>
+#import <TensorFlowLiteC/profiler.h>
+#import <TensorFlowLiteC/telemetry_setting.h>
+#import <TensorFlowLiteC/xnnpack_delegate.h>
